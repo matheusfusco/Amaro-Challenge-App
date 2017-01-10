5fbca7de4bb84a2dd7d67ceca19e34ca264b1c80
Matheus Pacheco Fusco <matheus.fusco@nc001374.local>
2017-01-10 10:44:17 -0200

    Initial Commit

---
+++ Amaro-Challenge-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amaro-Challenge-App
+//
+//  Created by Matheus Pacheco Fusco on 10/01/17.
+//  Copyright © 2017 MatheusFusco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
